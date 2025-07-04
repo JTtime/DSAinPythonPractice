7e589276fd36661a6640cf39f3165eb09a4562db
JTtime <105555233+JTtime@users.noreply.github.com>
2025-07-04 23:39:57 +0530

    edge case for binary search

--- binary_search_with_repeated_numbers.py
+++ binary_search_with_repeated_numbers.py
@@ -1,7 +1,12 @@
-number_list = [2,4,12,56,234,675,789, 799, 800, 812, 832]
+number_list = [4,4,4,4,4,4,4,4,4,12,56,234,675,789, 799, 800, 812, 832]
+
+
+
+
 
 def find_number(list_of_numbers, target):
     lo, hi = 0, len(list_of_numbers) - 1
+    result = -1
 
     while lo <= hi:
         mid=(lo+hi)//2
@@ -9,14 +14,15 @@ def find_number(list_of_numbers, target):
         print("Lo:", lo, "hi:", hi, "mid:", mid, "mid_number:", mid_number)
 
         if mid_number==target:
-            return mid
+            result = mid #instead of returning result directly, just save it
+            hi=mid-1 # keep looking on left side
         elif mid_number<target:
             lo=mid+1
         elif mid_number>target:
             hi=mid-1
     
 
-    return -1
+    return result
 
-ans = find_number(number_list, 4)
+ans = find_number(number_list, 5)
 print("ANS:", ans)
