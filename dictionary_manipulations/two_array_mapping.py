10a123d687428825f911c6be37145cdd5f0c8771
JTtime <105555233+JTtime@users.noreply.github.com>
2025-07-05 23:47:18 +0530

    hashing optimised for time complexity

--- dictionary_manipulations/two_array_mapping.py
+++ dictionary_manipulations/two_array_mapping.py
@@ -1,20 +1,49 @@
-n = [2,3,4,5,2,4,2,3,2,44,4,1,2,44,2,2,42,2,5]
+n = [2,3,4,5,2,4,2,3,2,44,4,1,2,4,2,2,4,2,5]
 
-m = [2,3,5,4,6,9]
+m = [2,3,4,6,9]
 
-ans_dic={}
+#brute force method
 
-for num in m:
-    count=0
-    for x in n:
-        if num==x:
-            count+=1            
-            # ans_dic[num]=ans_dic.get(num,0)+1   # uncommenting this line will ignore zero values in dictionary output, because it is inside if condition
+# ans_dic={}
+
+# for num in m:
+#     count=0
+#     for x in n:
+#         if num==x:
+#             count+=1            
+#             # ans_dic[num]=ans_dic.get(num,0)+1   # uncommenting this line will ignore zero values in dictionary output, because it is inside if condition
             
     
   
-    ans_dic[num]=count # this line includes zero values like 6 and 9 in dictionary
+#     ans_dic[num]=count # uncomenting this line includes zero values like 6 and 9 in dictionary
 
 
-print(ans_dic)
+# print(ans_dic)
+
+hash_list=[0]*11
+
+# result_hash={}
+
+# for num in m:
+#     result_hash[num] = n.count(num) time complexity is O(n * m)
+
+#following is time complexity O(n+m)
+for num in m:
+    hash_list[num]+=1
+
+for num in n:
+    if (1<=num<=10) and hash_list[num]!=0:
+        hash_list[num]+=1
+
+
+for num in m:
+    hash_list[num]-=1
+
+
+
+print("hash List",hash_list)
+
+
+
+
 
