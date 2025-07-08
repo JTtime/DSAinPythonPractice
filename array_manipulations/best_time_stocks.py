c8add9ffa863b852076d77af0d59640b490ccbaf
JTtime <105555233+JTtime@users.noreply.github.com>
2025-07-08 23:15:03 +0530

    optimal solution for best time for stock

--- array_manipulations/best_time_stocks.py
+++ array_manipulations/best_time_stocks.py
@@ -1,5 +1,7 @@
 # prices = [7,2,1,5,6,4,8]
-prices = [7, 1, 5, 3, 6, 4]
+# prices = [3, 2, 6, 1, 3]
+# prices = [3, 3, 3, 3, 3]
+prices = [2, 1, 2, 0, 1]
 
 #brute force
 def best_time(arr):
@@ -12,11 +14,26 @@ def best_time(arr):
                 max_profit=arr[j]-arr[i]
                 head=arr[i]
                 tail=arr[j]
-                
-
-
-    print("brute forcr:", max_profit, head, tail)       
-
-
-
+    print("brute forcr:", max_profit, head, tail)
 best_time(prices)
+
+def maximum_of_two(a,b):
+    if a>b:
+        return a
+    else:
+        return b
+
+def optimal_time(arr):
+    lowest_price=arr[0]
+    max_profit=float("-inf")
+    for num in arr:        
+        profit=num-lowest_price
+        max_profit=maximum_of_two(max_profit, profit)
+        if num<lowest_price:
+            lowest_price=num
+    
+
+    print("optimal solution:", max_profit)
+
+
+optimal_time(prices)
