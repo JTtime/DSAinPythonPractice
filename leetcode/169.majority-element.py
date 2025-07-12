a25784bcbb92a2d26551fecbbf56b2b125d074f5
JTtime <105555233+JTtime@users.noreply.github.com>
2025-07-12 22:10:46 +0530

    moore's voting algorithm in py

--- leetcode/169.majority-element.py
+++ leetcode/169.majority-element.py
@@ -7,9 +7,48 @@
 # @lc code=start
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        i=0
-        j=len(nums)
-        while (nums[i])
+       n = len(nums)
+
+       #Moore's voting algorithm
+
+       ans=nums[0]
+       count=0
+
+       for index, value in enumerate(nums):
+           if (count==0):
+               ans = value
+            
+           if (ans==value):
+               count+=1
+            
+           if (ans!=value):
+               count-=1
+        
+       count=0
+       for index, value in enumerate(nums):
+           if (ans==value):
+               count+=1
+            
+        
+       if (count>n/2):
+           return ans
+           
+               
+           
+
+
+       #Brute force passes only 42 of 53 test cases
+    #    for i, static_value in enumerate(nums):
+    #        frequency=0
+    #        for j, checking_value in enumerate(nums):
+    #            if static_value==checking_value:
+    #                frequency+=1
+                
+            
+    #        if frequency>(n/2):
+    #                return static_value
+                   
+               
         
 # @lc code=end
 
