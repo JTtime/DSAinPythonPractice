479b4fd07e3e26e64ced2710800656b85d72b56c
JTtime <105555233+JTtime@users.noreply.github.com>
2025-08-03 19:32:40 +0530

    single number leetcode easy python solution

---
+++ leetcode/136.single-number.py
@@ -0,0 +1,18 @@
+#
+# @lc app=leetcode id=136 lang=python3
+#
+# [136] Single Number
+#
+
+# @lc code=start
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        ans = 0
+        
+        for i in range(0, len(nums)):
+            ans = ans^nums[i]
+        
+        return ans
+        
+# @lc code=end
+
