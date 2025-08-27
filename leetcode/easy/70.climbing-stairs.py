89dd2e7dd873aa3c4d89f7e3e96ba382e646b499
JTtime <105555233+JTtime@users.noreply.github.com>
2025-08-27 15:59:14 +0530

    climbing stairs method 2 tabulation beats 100% py submissions

--- leetcode/easy/70.climbing-stairs.py
+++ leetcode/easy/70.climbing-stairs.py
@@ -7,15 +7,22 @@
 # @lc code=start
 class Solution:
     def climbStairs(self, n: int) -> int:
-        dp=[-1]*(n+2)
-        dp[n] = 1
-        dp[n+1] = 0
+        # if (n<1):
+            # return n+1
+        # if n==1:
+            # return 1
+         # return self.climbStairs(n-1) + self.climbStairs(n-2) 
         
         
-        for i in range(n-1, -1, -1):
-            dp[i] = dp[i+1] + dp[i+2]
+        dp = [-1]*(n+1)
+        dp[0] = 1
+        dp[1] = 1               
         
-        return dp[0]
+        for i in range(2, n+1):
+            dp[i] = dp[i-1] + dp[i-2]        
+        return dp[n]
+        
+        
             
         
 # @lc code=end
