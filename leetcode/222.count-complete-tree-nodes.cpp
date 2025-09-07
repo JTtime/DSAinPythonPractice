8c63be63e57900867dba2e090af25a5e826bd9ac
JTtime <105555233+JTtime@users.noreply.github.com>
2025-09-07 06:56:46 +0530

    count nodes of binary tree cpp beats 100% runtime

---
+++ leetcode/222.count-complete-tree-nodes.cpp
@@ -0,0 +1,35 @@
+/*
+ * @lc app=leetcode id=222 lang=cpp
+ *
+ * [222] Count Complete Tree Nodes
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int countNodes(TreeNode* root) {
+
+         if (root==NULL) {
+            return 0;
+        }
+        
+        int left = countNodes(root->left);
+        int right = countNodes(root->right);
+
+        return left + right + 1;
+        
+    }
+};
+// @lc code=end
+
