97b201f5d026769236bc24c372c5dcd730323b8d
JTtime <105555233+JTtime@users.noreply.github.com>
2025-09-07 14:39:25 +0530

    diameter of binary tree cpp solution beats 100% runtime and 96% memory usage

---
+++ leetcode/543.diameter-of-binary-tree.py
@@ -0,0 +1,34 @@
+#
+# @lc app=leetcode id=543 lang=python3
+#
+# [543] Diameter of Binary Tree
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def __init__ (self):
+        self.ans=0
+    def height(self, root):
+        if not root:
+            return 0
+        
+        left = self.height(root.left)
+        right = self.height(root.right)
+        
+        self.ans = max(self.ans, left + right)
+        
+        return 1+max(left, right)
+        
+        
+        
+    def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+        self.height(root)
+        return self.ans
+# @lc code=end
+
