1754f7fe51c192d1d7096a6ac67290cc202adfcf
JTtime <105555233+JTtime@users.noreply.github.com>
2025-09-07 07:34:15 +0530

    subtree cpp solution beats 100% runtime

---
+++ leetcode/572.subtree-of-another-tree.cpp
@@ -0,0 +1,51 @@
+/*
+ * @lc app=leetcode id=572 lang=cpp
+ *
+ * [572] Subtree of Another Tree
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isSubtree(TreeNode* root, TreeNode* subRoot) {
+
+        if (root==NULL || subRoot==NULL) {
+            return root==subRoot;
+        }
+
+         else if ((root->val==subRoot->val) && isSameTree(root, subRoot)) {
+            return true;
+        }
+
+        bool isLeftSame = isSubtree(root->left, subRoot);
+        bool isRightSame = isSubtree(root->right, subRoot);
+
+        return isLeftSame || isRightSame || isSameTree(root, subRoot);        
+    }
+
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+         if (p==NULL || q==NULL) {
+            return p==q;
+        }
+        
+        bool isLeftSame = isSameTree(p->left, q->left);
+        bool isRightSame = isSameTree(p->right, q->right);
+
+        return isLeftSame && isRightSame && (p->val == q->val);
+    }
+
+
+};
+// @lc code=end
+
