f4712b4c872987ac22ca63f0805c8ce2060640ca
JTtime <105555233+JTtime@users.noreply.github.com>
2025-12-26 18:25:51 +0530

    sort colors py beats 100% runtime and 98% memory

---
+++ leetcode/75.sort-colors.py
@@ -0,0 +1,32 @@
+#
+# @lc app=leetcode id=75 lang=python3
+#
+# [75] Sort Colors
+#
+
+# @lc code=start
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        count_red, count_white, count_blue = 0, 0, 0
+
+        for num in nums:
+            if num == 0:
+                count_red+=1
+            elif num == 1:
+                count_white+=1
+            elif num == 2:
+                count_blue+=1
+        
+        for i in range(count_red):
+            nums[i] = 0
+        for i in range(count_red, count_red+count_white):
+            nums[i] = 1
+        for i in range(count_red+count_white, count_red+count_white+count_blue):
+            nums[i] = 2
+        
+
+# @lc code=end
+
