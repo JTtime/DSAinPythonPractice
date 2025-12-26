94cfa8a19e3b7782c5605325da15cf34fda2ece8
JTtime <105555233+JTtime@users.noreply.github.com>
2025-12-26 18:26:09 +0530

    two sum 2 py solution

---
+++ leetcode/167.two-sum-ii-input-array-is-sorted.py
@@ -0,0 +1,21 @@
+#
+# @lc app=leetcode id=167 lang=python3
+#
+# [167] Two Sum II - Input Array Is Sorted
+#
+
+# @lc code=start
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        i, j = 0, len(numbers) - 1
+        
+        while i < j:
+            if (numbers[i] + numbers[j]) == target:
+                return [i+1, j+1]
+            elif (numbers[i] + numbers[j]) > target:
+                j-=1
+            else:
+                i+=1
+        
+# @lc code=end
+
