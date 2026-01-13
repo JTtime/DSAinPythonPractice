ec117066c451f9495917cc961a8e0f2dde3a06fb
JTtime <105555233+JTtime@users.noreply.github.com>
2026-01-13 13:18:16 +0530

    fibbo 3 ways i py

--- leetcode/easy/509.fibonacci-number.py
+++ leetcode/easy/509.fibonacci-number.py
@@ -7,17 +7,41 @@
 # @lc code=start
 class Solution:
     def fib(self, n: int) -> int:
+        # recursive approach
+        # if n<=1:
+        #     return n
+        # return self.fib(n-1) + self.fib(n-2)
         
-        dp = [-1]*(n+1)
+        #dp top down approach        
         
-        for i in range(n+1):
-            if i<2:
-                dp[i]=i
-            else:
-                dp[i]=dp[i-1]+dp[i-2]
+        # dp = [-1]*(n+1)        
+        
+        # def helper(n, dp):
+        #     if n<=1:
+        #         return n
+        #     if dp[n] != -1:
+        #         return dp[n]
+        #     dp[n] = helper(n-1, dp) + helper(n-2, dp)
+        #     return dp[n]
+        
+        # return helper(n, dp)       
+        
+        
+        #dp bottom up approach
+        # dp = [-1]*(n+1)
+        # if n<=1:
+        #     return n
+        # dp[0] = 0
+        # dp[1] = 1      
+        
+        
+        # for i in range(2, n+1):
+        #     dp[i] = dp[i-1] + dp[i-2]        
+        # return dp[n]
+        
+        
+        
             
-        
-        return dp[n]
         
 # @lc code=end
 
